7d5ffbe0e28e07d226854cf22aacc93fc2822dca
Naser Ahadi <naserahadi95@gmail.com>
2022-10-30 09:14:56 +0330

    ch2/refactor: change the sanity test by using vue/test-utils 'mount' method

--- src/components/__tests__/ItemComponent.spec.js
+++ src/components/__tests__/ItemComponent.spec.js
@@ -1,13 +1,9 @@
+import {mount} from "@vue/test-utils"
 import ItemComponent from '../ItemComponent.vue'
-import Vue from "vue";
 
 describe('ItemComponent.vue', () => {
     test('renders "item"', () => {
-        const Ctor = Vue.extend(ItemComponent)          // create a new Vue constructor with the ItemComponent options
-        const vm = new Ctor().$mount()                  // create a new Vue instance, and mounts the Vue instance
-        expect(vm.$el.textContent).toContain('item')  // access the DOM element, and checks the text content
+        const wrapper = mount(ItemComponent)
+        expect(wrapper.vm.$el.textContent).toContain('item')
     })
 })
-
-// above test fail because el option is undefined
-// solve above problem with installing jest with vue cli and change name of directory __test__ to __tests__

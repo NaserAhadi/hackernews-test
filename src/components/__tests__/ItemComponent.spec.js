e8a314ef441b351bc1574048e57b48241cb92ba6
Naser Ahadi <naserahadi95@gmail.com>
2022-11-06 13:16:31 +0330

    test: check ItemComponent renders item.url

--- src/components/__tests__/ItemComponent.spec.js
+++ src/components/__tests__/ItemComponent.spec.js
@@ -2,8 +2,14 @@ import { shallowMount } from "@vue/test-utils"
 import ItemComponent from '../ItemComponent.vue'
 
 describe('ItemComponent.vue', () => {
-    test('renders "item"', () => {
-        const wrapper = shallowMount(ItemComponent)
-        expect(wrapper.text()).toContain('item')
+    test('renders item.url', () => {
+        const item = {
+            url: "10",
+        }
+        const wrapper = shallowMount(ItemComponent, {
+            propsData: { item }
+        })
+        console.log('here', wrapper.text())
+        expect(wrapper.text()).toContain(item.url)
     })
 })

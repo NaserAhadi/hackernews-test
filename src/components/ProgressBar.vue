63369ffb16a8d0eadd9f689929f8710cef374478
Naser Ahadi <naserahadi95@gmail.com>
2022-12-11 12:24:47 +0330

    ch4/test(methods): write test for dynamic css class

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,3 +1,19 @@
 <template>
-    <div class="hidden" :style="{ 'width': '0%'}" ></div>
+    <div :class="{'hidden': hidden}" :style="{ 'width': '0%'}" ></div>
 </template>
+
+<script>
+export default{
+    data(){
+        return {
+            hidden: true
+        }
+    },
+    methods:{
+        start(){
+            this.hidden = false
+        },
+        finish(){}
+    },
+}
+</script>
--- src/components/__tests__/ProgressBar.spec.js
+++ src/components/__tests__/ProgressBar.spec.js
@@ -5,6 +5,9 @@ describe('ProgressBar.vue', () => {
     test('is hidden on initial render', () => {
         const wrapper = shallowMount(ProgressBar)
         expect(wrapper.classes()).toContain('hidden')
+        // it doesn't work, doesn't read method!! but why?
+        wrapper.vm.start()
+        expect(wrapper.classes()).not.toContain('hidden')
     })
 
     test('initilizes with 0% width', () => {

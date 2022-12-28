d15670e040b51f84845323d391d1d8b27aacf31b
Naser Ahadi <naserahadi95@gmail.com>
2022-12-28 10:07:38 +0330

    ch4/ feat: check progress bar width and check clearInterval

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -7,17 +7,29 @@ export default{
     data(){
         return {
             hidden: true,
-            width: 0
+            width: 0,
+            timer:0
         }
     },
     methods:{
         start(){
             this.hidden = false
             this.width = 0
+            this.timer = setInterval(() => {
+                    this.width ++
+                    console.log(this.width)
+                }, 100)
+            // this.$nextTick(() => {
+            //     this.timer = setTimeout(() => {
+            //         this.width ++
+            //         console.log(this.width)
+            //     }, 100)
+            // })
         },
         finish(){
             this.width = 100
             this.hidden = true
+            clearInterval(this.timer)
         }
     },
 }
--- src/components/__tests__/ProgressBar.spec.js
+++ src/components/__tests__/ProgressBar.spec.js
@@ -3,6 +3,10 @@ import Vue from 'vue'
 import ProgressBar from '../ProgressBar'
 
 describe('ProgressBar.vue', () => {
+    // beforeEach(() => {
+    //     jest.useFakeTimers()
+    // })
+
     test('is hidden on initial render', async () => {
         const wrapper = shallowMount(ProgressBar)
         expect(wrapper.classes()).toContain('hidden')
@@ -36,5 +40,29 @@ describe('ProgressBar.vue', () => {
         wrapper.vm.finish()
         wrapper.vm.start()
         expect(wrapper.element.style.width).toBe('0%')
+    })
+
+    test('increases width by 1% every 100ms after start call', async () => {
+        jest.useFakeTimers()
+
+        const wrapper = shallowMount(ProgressBar)
+        wrapper.vm.start()
+        await Vue.nextTick()
+        jest.advanceTimersByTime(100)
+        expect(wrapper.element.style.width).toBe('1%')
+        // jest.advanceTimersByTime(900)
+        // expect(wrapper.element.style.width).toBe('10%')
+        // jest.advanceTimersByTime(4000)
+        // expect(wrapper.element.style.width).toBe('50%')
+    })
+
+    test('clears timer when finish is called', () => {
+        jest.useFakeTimers()
+        jest.spyOn(window, 'clearInterval')
+        setInterval.mockReturnValue(123)
+        const wrapper = shallowMount(ProgressBar)
+        wrapper.vm.start()
+        wrapper.vm.finish()
+        expect(window.clearInterval).toHaveBeenCalledWith(123)
     })
 })

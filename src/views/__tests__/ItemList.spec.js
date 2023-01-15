68527442ca53e62cca6a76d6785468dd711998c3
Naser Ahadi <naserahadi95@gmail.com>
2023-01-15 19:51:34 +0330

    ch4/test: remove tests which is based on fetching data and setting them on window property | add new tests for fetching data in ItemList

--- src/views/__tests__/ItemList.spec.js
+++ src/views/__tests__/ItemList.spec.js
@@ -1,22 +1,57 @@
 import {shallowMount} from '@vue/test-utils'
 import ItemList from '../ItemList'
 import Item from '../../components/ItemComponent'
+import flushPromises from 'flush-promises'
+import { fetchListData } from '../../api/api'   
+
+jest.mock('../../api/api.js')
 
 
 describe('ItemList.vue', () => {
-    test('renders Item component for each item object in window.items', () => {
-        window.items = [{}, {}, {}]
-        const wrapper = shallowMount(ItemList)
-        expect(wrapper.findAllComponents(Item)).toHaveLength(window.items.length)
+    test('calls $bar start on load', () => {
+        const $bar = {
+            start: jest.fn(),
+            finish: () => {}
+        }
+
+        shallowMount(ItemList, {
+            mocks: {$bar}
+        })
+
+        expect($bar.start).toHaveBeenCalledTimes(1)
     })
 
-    test('check Item component receive prop', () => {
-        window.items = [{}, {}, {}]
-        const wrapper = shallowMount(ItemList)
-        const items = wrapper.findAllComponents(Item) 
-       
-        items.wrappers.forEach((wrapper, i) => {
-            expect(wrapper.props().item).toBe(window.items[i])
+    test('renders an Item with data for each item', async() => {
+        expect.assertions(4)
+        const $bar = {
+            start: () => {},
+            finish: () => {}
+        }
+
+        const items = [{id:1},{id:2},{id:3}]
+        fetchListData.mockResolvedValueOnce(items)
+
+        const wrapper = shallowMount(ItemList, {mocks: {$bar}})
+
+        await flushPromises()
+        const Items = wrapper.findAllComponents(Item)
+        expect(Items).toHaveLength(items.length)
+
+        Items.wrappers.forEach((wrapper, i) => {
+            expect(wrapper.vm.item).toBe(items[i])
         })
+    })
+
+    test('calls $bar.finish when load is successful', async() => {
+        expect.assertions(1)
+
+        const $bar = {
+            start: () => {},
+            finish: jest.fn()
+        }
+
+        shallowMount(ItemList, {mocks: {$bar}})
+        await flushPromises()
+        expect($bar.finish).toHaveBeenCalled()
     })
 })

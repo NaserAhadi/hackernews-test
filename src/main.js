f7e36b5ecb382389ec288c861f5dd54b8abdfbf1
Naser Ahadi <naserahadi95@gmail.com>
2023-01-15 19:46:44 +0330

    ch4/refactor: add ProgressBar as a property to Vue instance | change the way getting and setting data
    mounted ProgressBar component is added to Vue instance so other component can access it.
    we don't set fetched data to property of window anymore

--- src/main.js
+++ src/main.js
@@ -1,23 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import router from './router'
-import { fetchListData } from './api/api'
+import ProgressBar from './components/ProgressBar'
+
 
 Vue.config.productionTip = false
 
-function getTopItems () {
-  return fetchListData('top')
-    .then(items => items)
-}
-// new Vue({
-//   router,
-//   render: h => h(App)
-// }).$mount('#app')
+const bar = new Vue(ProgressBar).$mount()
+Vue.prototype.$bar = bar
+document.body.appendChild(bar.$el)
 
-getTopItems().then((items) => {
-  window.items = items
-  new Vue({
-    el: '#app',
-    render: h => h(App)
-  })
+new Vue({
+  el: '#app',
+  render: h => h(App)
 })
